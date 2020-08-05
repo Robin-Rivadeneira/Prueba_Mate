b90326f17fb7efeac5e952453d685a0ffd1d5602
Robin-Rivadeneira <48358672+Robin-Rivadeneira@users.noreply.github.com>
2020-08-05 10:54:26 -0500

    a

--- calculos.js
+++ calculos.js
@@ -1,100 +1,115 @@
-document.getElementById("SI").style.display = "none"
-document.getElementById("NO").style.display = "none"
-var ingreso = document.getElementById("ingreso");
-var ingresos = document.getElementById("mostrar");
-var mensaje = document.getElementById("mensaje");
-
-var num = []
-var repetido = []
-var unico = []
-var unido = []
-unido.sort()
-var repetidos = {}
-var valor=[]
-var sumaT = 0
-var sumaFr = 0
-var sumaPorcentual = 0
-var frecuenciaPorcentual = []
-var frecuenciaRelativa = []
-var frecuenciaAcumulada = []
-function Mostrar() {
+class calculos{
+    constructor(){
+    this.ingreso = document.getElementById("ingreso");
+    this.ingresos = document.getElementById("mostrar");
+    this.mensaje = document.getElementById("mensaje");
+    this.si=document.getElementById("SI").style.display = "none"
+    this.no=document.getElementById("NO").style.display = "none"
+    this.num = []
+    this.frecuenciaPorcentual = []
+    this.frecuenciaRelativa = []
+    this.frecuenciaAcumulada = []
+    this.repetidos = {}
+    this.valor = []
+    this.cont=1
+    this.sumaT = 0
+    this.sumaFr = 0
+    this.sumaPorcentual = 0
+    }
+Mostrar() {
     document.getElementById("ocular").style.display = "none"
-    document.getElementById("SI").style.display = "block"
+    document.getElementById("SI").style.display= "block"
     document.getElementById("NO").style.display = "block"
-    mensaje.style.display = "block"
+    this.mensaje.style.display = "block"
     mensaje.innerHTML = '<p>Ingresar otro valor</p>'
-    num.push(ingreso.value)
+    this.num.push(this.ingreso.value)
 }
-function Si() {
+Si() {
     document.getElementById("SI").style.display = "none"
     document.getElementById("NO").style.display = "none"
     document.getElementById("ocular").style.display = "block"
-    mensaje.style.display = "none"
-    console.log(num.length)
+    this.mensaje.style.display = "none"
+    console.log(this.num.length)
 }
-function No() {
+No() {
     document.getElementById("SI").style.display = "none"
     document.getElementById("NO").style.display = "none"
-    mensaje.style.display = "none"
+    this.mensaje.style.display = "none"
     document.getElementById("ocular").style.display = "block"
-    document.getElementById("ms").innerHTML = num.toString();
+    document.getElementById("ms").innerHTML = this.num.toString();
 
-    for (let j = 0; j < num.length; j++) {
-        for (let k = j + 1; k < num.length; k++) {
-            if (num[j] == num[k]) {
-                repetido.push(num[k])
-            }
+    var uniqs = this.num.filter(function (item, index, array) {
+        return array.indexOf(item) === index;
+    })
+    console.log(uniqs);
+    document.getElementById("unido").innerHTML = uniqs.toString()
+
+    for(let i=0;i<this.num.length;i++){
+        for(let j=i+1;j<this.num.length;j++){
+            if(this.num[i]==this.num[j]){
+                let b=this.cont+1
+                this.valor.push(this.cont)
+                this.valor.push(b)
+                console.log(this.valor)
         }
+      }
     }
-    for (let a = 0; a < num.length; a++) {
-        for (let b = 0; b < repetido.length; b++) {
-            if (num[a] != repetido[b]) {
-                unico.push(num[a])
-            }
-        }
+
+    for(let i=0;i<this.valor.length;i++){
+        this.sumaT=this.sumaT+this.valor[i]
+        console.log(this.sumaT)
     }
-    unido = unico.concat(repetido)
-    document.getElementById("unido").innerHTML = unido.toString()
-
-    num.forEach(function (numero) {
-        repetidos[numero] = (repetidos[numero] || 0) + 1;
-        valor.push(repetidos[numero])
-    });
-    console.log(valor);
-
-    valor.forEach(function (numero) {
-        sumaT += numero;
-    });
-    console.log(sumaT);
-
-    for (let c = 0; c < valor.length; c++) {
-        let fr = valor[c] / sumaT
-        frecuenciaRelativa.push(fr)
+    
+    for (let c = 0; c < this.valor.length; c++) {
+        let fr = this.valor[c] / this.sumaT
+        this.frecuenciaRelativa.push(fr)
         console.log(fr)
     }
 
-    frecuenciaRelativa.forEach(function (numero) {
-        sumaFr += numero;
-    });
-    console.log(sumaFr)
+    for(let i=0;i<this.frecuenciaRelativa.length;i++){
+        this.sumaFr=this.sumaFr+this.frecuenciaRelativa[i]
+        console.log(this.sumaFr)
+    }
 
-    for (let d = 0; d < frecuenciaRelativa.length; d++) {
-        let fp = frecuenciaRelativa[d] * 100
-        frecuenciaPorcentual.push(fp)
+    for (let d = 0; d < this.frecuenciaRelativa.length; d++) {
+        let fp = this.frecuenciaRelativa[d] * 100
+        this.frecuenciaPorcentual.push(fp)
         console.log(fp)
     }
 
-    frecuenciaPorcentual.forEach(function (numero) {
-        sumaPorcentual += numero;
-    });
-    console.log(sumaPorcentual)
-    
-    for(m=0;m<valor.length;m++){
-            let a = valor[m]
-            let b = valor[m]
-            frecuenciaAcumulada=a+b+1
-            console.log(frecuenciaAcumulada)
-            a=b
-            b=frecuenciaAcumulada
+    for(let i=0;i<this.frecuenciaPorcentual.length;i++){
+        this.sumaPorcentual=this.sumaPorcentual+this.frecuenciaPorcentual[i]
+        console.log(this.sumaPorcentual)
     }
+
+    for (let m = 0; m < this.valor.length; m++) {
+        let a = this.valor[m]
+        let b = this.valor[m]
+        let c = a + b-1
+        console.log(this.frecuenciaAcumulada)
+        a = b
+        b = c
+        this.frecuenciaAcumulada.push(c)
+    }
+
+    var codigohtml = "<table border='1'><tbody>"
+    codigohtml += "<tr>"
+    codigohtml += "<td>Frecuencias</td>"
+    codigohtml += "<td>Frecuencia Relativa</td>"
+    codigohtml += "<td>Frecuencia Porcentual</td>"
+    codigohtml += "<td>Frecuencia Acumulada</td>"
+    codigohtml += "</tr>"
+    codigohtml += "<tr>"
+    for (let g = 0; g < uniqs.length; g++) {
+        codigohtml += "<td>" + this.valor[g] + "</td>"
+        codigohtml += "<td>" + this.frecuenciaRelativa[g] + "</td>"
+        codigohtml += "<td>" + this.frecuenciaPorcentual[g] + "</td>"
+        codigohtml += "<td>" + this.frecuenciaAcumulada[g] + "</td>"
+        codigohtml += "</tr>"
+    }
+    codigohtml += "</tbody></table>"
+    document.getElementById("table").innerHTML = codigohtml
 }
+}
+
+let a=new calculos()
